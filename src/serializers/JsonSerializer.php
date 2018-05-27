02cae6f953871e4227257341f78c0bea6d57eb75
Matrozov <oleg.matrozov@gmail.com>
2018-05-27 15:36:01 +0300

    Fix jsonSerializer

--- src/serializers/JsonSerializer.php
+++ src/serializers/JsonSerializer.php
@@ -30,31 +30,16 @@ class JsonSerializer implements Serializer
     }
 
     /**
-     * @param $data
+     * @param array $data
      *
      * @return array
      * @throws
      */
-    protected function toArray($data)
+    protected function iterateArray($data)
     {
         $result = [];
 
-        if ($data instanceof Model) {
-            $result['scenario'] = $data->getScenario();
-
-            $arrayData = $data->toArray();
-        }
-        elseif (is_object($data)) {
-            $arrayData = get_object_vars($data);
-        }
-        elseif (is_array($data)) {
-            $arrayData = $data;
-        }
-        else {
-            return $data;
-        }
-
-        foreach ($arrayData as $key => $value) {
+        foreach ($data as $key => $value) {
             if ($key === 'class') {
                 throw new ErrorException('Object can\'t contain `class` property!');
             }
@@ -62,9 +47,37 @@ class JsonSerializer implements Serializer
             $result[$key] = self::toArray($value);
         }
 
-        $result['class'] = get_class($data);
-
         return $result;
+    }
+
+    /**
+     * @param $data
+     *
+     * @return array
+     * @throws
+     */
+    protected function toArray($data)
+    {
+        if ($data instanceof Model) {
+            $result = $this->iterateArray($data->toArray());
+
+            $result['scenario'] = $data->getScenario();
+            $result['class'] = get_class($data);
+
+            return $result;
+        }
+        elseif (is_object($data)) {
+            $result = $this->iterateArray(get_object_vars($data));
+
+            $result['class'] = get_class($data);
+
+            return $result;
+        }
+        elseif (is_array($data)) {
+            return $this->iterateArray($data);
+        }
+
+        return $data;
     }
 
     /**
