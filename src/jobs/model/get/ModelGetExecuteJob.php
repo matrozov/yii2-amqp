3762f9ae4cd88ff28e91296e610af59120b3979b
Matrozov <oleg.matrozov@gmail.com>
2018-06-12 16:59:13 +0300

    Fix ModelGetExecuteJob

--- src/jobs/model/get/ModelGetExecuteJob.php
+++ src/jobs/model/get/ModelGetExecuteJob.php
@@ -1,13 +1,11 @@
 <?php
 namespace matrozov\yii2amqp\jobs\model\get;
 
-use matrozov\yii2amqp\jobs\rpc\RpcExecuteJob;
-
 /**
  * Interface ModelGetExecuteJob
  * @package matrozov\yii2amqp\jobs\model\find
  */
-interface ModelGetExecuteJob extends RpcExecuteJob
+interface ModelGetExecuteJob
 {
     public static function executeFindOne($conditions);
 }
